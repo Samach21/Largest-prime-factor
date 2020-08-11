7b6eebf5be66816913ae729735612666022963e5
Samach21 <69465282+Samach21@users.noreply.github.com>
2020-08-11 23:41:10 +0700

    Add project files.

---
+++ Largest prime factor/Largest prime factor.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main()
+{
+	long n = 600851475143, max;
+	int x[4000];
+	x[0] = 0;
+	for (long i = 1; i <= 4000; i++)
+	{
+		if (n % i == 0)
+		{
+			n = n / i;
+			x[i] = i;
+			if (x[i] > x[i - 1])
+			{
+				max = x[i];
+			}
+			else
+			{
+				max = x[i-1];
+			}
+		}
+		else
+		{
+			x[i] = 1;
+		}
+	}
+	printf("Result: %d", max);
+	return 0;
+}
